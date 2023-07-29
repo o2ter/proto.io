4cd116a224bddd040fba2367fb97a1c653b9d0a4
Susan Cheng <susan.doggie@gmail.com>
2023-07-29 14:29:46 +0800

    Update schedule.ts

--- src/server/schedule.ts
+++ src/server/schedule.ts
@@ -33,6 +33,7 @@ const scheduleOp = {
       await storage.findAndDelete({
         className,
         filter: QuerySelector.decode({ _expired_at: { $lt: new Date() } }),
+        includes: []
       });
     }
   }
