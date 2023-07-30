98375ebeb658298c08f19671ec57365892de82ad
Susan Cheng <susan.doggie@gmail.com>
2023-07-30 20:58:42 +0800

    Update storage.ts

--- src/server/sql/storage.ts
+++ src/server/sql/storage.ts
@@ -135,7 +135,7 @@ export abstract class SqlStorage implements TStorage {
     });
 
     const result = this.query(sql`
-      INSERT INTO films (${_.map(_attrs, x => sql`${{ identifier: x[0] }}`)})
+      INSERT INTO ${{ identifier: options.className }} (${_.map(_attrs, x => sql`${{ identifier: x[0] }}`)})
       VALUES (${_.map(_attrs, x => sql`${{ value: x[1] }}`)})
       RETURNING *
     `);
