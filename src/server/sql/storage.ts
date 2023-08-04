621cc989cc5c49333c111d077ea22686b944d8d3
Susan Cheng <susan.doggie@gmail.com>
2023-08-04 12:32:30 +0800

    Update storage.ts

--- src/server/sql/storage.ts
+++ src/server/sql/storage.ts
@@ -221,7 +221,7 @@ export abstract class SqlStorage implements TStorage {
     return _.isNil(result) ? undefined : this._decodeObject(options.className, result);
   }
 
-  protected _selectOneQuery(query: DecodedQuery<FindOneOptions>, compiler: QueryCompiler, action: (
+  protected _modifyQuery(query: DecodedQuery<FindOneOptions> & { limit?: number }, compiler: QueryCompiler, action: (
     tempName: string,
     populates: {
       column: SQL;
@@ -253,7 +253,7 @@ export abstract class SqlStorage implements TStorage {
       FROM ${{ identifier: query.className }} AS ${{ identifier: tempName }}
       ${!_.isEmpty(_joins) ? _joins : sql``}
       ${_filter ? sql`WHERE ${_filter}` : sql``}
-      LIMIT 1
+      ${query.limit ? sql`LIMIT ${{ literal: `${query.limit}` }}` : sql``}
     `;
 
     return sql`
