7c143a3d337060f478aae7f8aefef83bf96bfccc
Susan Cheng <susan.doggie@gmail.com>
2023-08-08 10:52:37 +0800

    Update dialect.ts

--- src/adapters/storage/progres/dialect.ts
+++ src/adapters/storage/progres/dialect.ts
@@ -349,7 +349,7 @@ export const PostgresDialect: SqlDialect = {
                   THEN ${element} ${{ literal: operatorMap[expr.type] }} ${encodeValue(expr.value)}
                 WHEN (jsonb_typeof(${element} -> '$decimal') ${this.nullSafeEqual()} 'string')
                   THEN to_jsonb((${element} ->> '$decimal')::DECIMAL) ${{ literal: operatorMap[expr.type] }} ${encodeValue(expr.value)}
-                ELSE false
+                ELSE ${element} ${{ literal: operatorMap[expr.type] }} ${encodeValue(expr.value)}
               END
             )
             `;
