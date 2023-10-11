d52034e24833b8b7b03aff01927714c78f00f05a
Susan Cheng <susan.doggie@gmail.com>
2023-10-11 17:23:24 +0800

    Update index.ts

--- src/server/proto/index.ts
+++ src/server/proto/index.ts
@@ -226,7 +226,7 @@ export class ProtoService<Ext> extends ProtoType<Ext> {
   }
 
   async publish(channel: string, payload: Record<string, _TValue>, options?: ExtraOptions) {
-    const roles = await this.roles();
+    const roles = _.uniq(_.compact([..._.map(await this.roles(), x => `role:${x}`), (await this.user())?.objectId]));
     const perm = this[PVK].options.channelPublishPermissions[channel] ?? ['*'];
     if (!options?.master && _.every(perm, x => x !== '*' && !_.includes(roles, x))) throw Error('No permission');
     this[PVK].publish(channel, {
