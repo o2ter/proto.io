7ab7a24b01ea1379cfd440a35bb6069e6a2c37ae
Susan Cheng <susan.doggie@gmail.com>
2024-06-04 11:10:15 +0800

    Update index.ts

--- src/server/proto/index.ts
+++ src/server/proto/index.ts
@@ -49,7 +49,7 @@ export class ProtoService<Ext> extends ProtoType<Ext> {
   private _schedule = schedule(this);
 
   req?: Request;
-  _session?: Session;
+  session?: Session;
 
   constructor(options: ProtoServiceOptions<Ext> & ProtoServiceKeyOptions) {
     super();
@@ -96,18 +96,18 @@ export class ProtoService<Ext> extends ProtoType<Ext> {
   }
 
   get sessionId(): string | undefined {
-    if (this._session) return this._session.sessionId;
+    if (this.session) return this.session.sessionId;
     return this.req ? sessionId(this, this.req) : undefined;
   }
 
   async currentUser() {
-    if (this._session) return this._session.user;
+    if (this.session) return this.session.user;
     const _session = this.req ? await session(this, this.req) : undefined;
     return _session?.user;
   }
 
   async currentRoles() {
-    if (this._session) return this._session.roles ?? [];
+    if (this.session) return this.session.roles ?? [];
     const _session = this.req ? await session(this, this.req) : undefined;
     return _session?.roles ?? [];
   }
@@ -130,9 +130,9 @@ export class ProtoService<Ext> extends ProtoType<Ext> {
 
   connectWithSession<T extends object>(
     session: Session,
-    attrs?: T | ((x: this & { _session: Session; }) => T)
-  ): this & { _session: Session; } & T {
-    const payload = _.create(this, { _session: session });
+    attrs?: T | ((x: this & { session: Session; }) => T)
+  ): this & { session: Session; } & T {
+    const payload = _.create(this, { session });
     return _.assign(payload, _.isFunction(attrs) ? attrs(payload) : attrs)
   }
 
