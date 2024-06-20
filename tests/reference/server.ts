2d824ed76ad8e2735d04724d7758242b085042d7
Susan Cheng <susan.doggie@gmail.com>
2024-06-20 11:57:24 +0800

    add test case

---
+++ tests/reference/server.ts
@@ -0,0 +1,106 @@
+//
+//  server.ts
+//
+//  The MIT License
+//  Copyright (c) 2021 - 2024 O2ter Limited. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import _ from 'lodash';
+import { Server } from '@o2ter/server-js';
+import { ProtoService, ProtoRoute } from '../../src/index';
+import { beforeAll, afterAll } from '@jest/globals';
+import DatabaseFileStorage from '../../src/adapters/file/database';
+import PostgresStorage from '../../src/adapters/storage/progres';
+import { randomUUID } from '@o2ter/crypto-js';
+
+const app = new Server;
+
+const db_host = process.env['POSTGRES_HOST'] ?? "localhost";
+const db_user = process.env['POSTGRES_USERNAME'];
+const db_pass = process.env['POSTGRES_PASSWORD'];
+const db = `/${process.env['POSTGRES_DATABASE'] ?? ''}`;
+const db_ssl = process.env['POSTGRES_SSLMODE'];
+
+const uri = _.compact([
+  'postgres://',
+  db_user && db_pass && `${db_user}:${db_pass}@`,
+  db_host, db,
+  db_ssl && `?ssl=true&sslmode=${db_ssl}`
+]).join('');
+
+const database = new PostgresStorage(uri);
+
+export const masterUser = {
+  user: 'admin',
+  pass: randomUUID(),
+};
+
+const Proto = new ProtoService({
+  endpoint: 'http://localhost:8080/proto',
+  masterUsers: [masterUser],
+  jwtToken: randomUUID(),
+  schema: {
+    'Refs': {
+      fields: {
+        pointer: { type: 'pointer', target: 'Refs' },
+        relation: { type: 'relation', target: 'Refs' },
+      }
+    },
+  },
+  storage: database,
+  fileStorage: new DatabaseFileStorage(),
+});
+
+Proto.define('testRefs', async (proto) => {
+
+  const obj = proto.Object('Refs');
+  await obj.save();
+
+  const result = proto.Object('Refs');
+  result.set('pointer', obj);
+  await result.save();
+
+  const [check] = await proto.nonrefs('Refs');
+  const [check2] = await proto.refs(obj);
+
+  return {
+    check,
+    check2,
+    result,
+  };
+});
+
+beforeAll(async () => {
+
+  app.use('/proto', await ProtoRoute({
+    proto: Proto,
+  }));
+
+  console.log('version: ', await database.version());
+
+  app.listen(8080, () => console.log('listening on port 8080'));
+});
+
+afterAll(async () => { 
+  await Proto.shutdown();
+  await database.shutdown();
+  await app.close();
+});
