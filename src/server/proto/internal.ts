e6fa0806cf37116e005c33f6c3b1615aa2065539
Susan Cheng <susan.doggie@gmail.com>
2025-01-03 17:34:03 +0800

    Update internal.ts

--- src/server/proto/internal.ts
+++ src/server/proto/internal.ts
@@ -520,7 +520,7 @@ export class ProtoInternal<Ext, P extends ProtoService<Ext>> implements ProtoInt
         });
 
         const job = await proto.Query('_Job')
-          .equalTo('status', 'pending')
+          .equalTo('status', ['pending', 'started'])
           .containsIn('name', _.keys(availableJobs))
           .empty('locks')
           .includes('*', 'user')
